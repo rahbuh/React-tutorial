cf02eb516a9408fda8108ac0e008b6ba07453a38
Robert Kramer <robert.n.kramer@gmail.com>
2019-07-16 16:12:07 -0700

    added class component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,64 @@
-import React from "react";
+import React, {Component} from "react";
 import ContactCard from "./components/ContactCards";
 import contactData from "./contactData";
 
-class MyApp extends React.Component {
+class App extends Component {
+  render() {
+    const contactComponents = contactData.map(contact => {
+      return (
+        <ContactCard 
+        key={contact.id} 
+        name={contact.name} 
+        imgUrl={contact.imgUrl} 
+        imgAlt={contact.imgAlt}
+        phone={contact.phone}
+        email={contact.email} />
+      )
+    });
 
+    return (
+      <div className="contacts">
+        {contactComponents}
+      </div>
+    )
+  }
 }
 
-const App = () => {
-  const contactComponents = contactData.map(contact => {
-    return (
-      <ContactCard 
-      key={contact.id} 
-      name={contact.name} 
-      imgUrl={contact.imgUrl} 
-      imgAlt={contact.imgAlt}
-      phone={contact.phone}
-      email={contact.email} />
-    )
-  });
+// const App = () => {
+//   const contactComponents = contactData.map(contact => {
+//     return (
+//       <ContactCard 
+//       key={contact.id} 
+//       name={contact.name} 
+//       imgUrl={contact.imgUrl} 
+//       imgAlt={contact.imgAlt}
+//       phone={contact.phone}
+//       email={contact.email} />
+//     )
+//   });
 
-  return (
-    <div className="contacts">
-      {/* <ContactCard
-        name="Mr. Whiskerson"
-        imgUrl="http://placekitten.com/300/200"
-        imgAlt="Picture of Mr. Whiskerson"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
-      /> INDIVIDUAL PROPS */}
-      {/* <ContactCard
-        contact={{
-          name: "Mr. Whiskerson",
-          imgUrl: "http://placekitten.com/300/200",
-          imgAlt: "Picture of Mr. Whiskerson",
-          phone: "(212) 555-1234",
-          email: "mr.whiskaz@catnap.meow"
-        }}
-      /> OBJECT PROPS */}
+//   return (
+//     <div className="contacts">
+//       {/* <ContactCard
+//         name="Mr. Whiskerson"
+//         imgUrl="http://placekitten.com/300/200"
+//         imgAlt="Picture of Mr. Whiskerson"
+//         phone="(212) 555-1234"
+//         email="mr.whiskaz@catnap.meow"
+//       /> INDIVIDUAL PROPS */}
+//       {/* <ContactCard
+//         contact={{
+//           name: "Mr. Whiskerson",
+//           imgUrl: "http://placekitten.com/300/200",
+//           imgAlt: "Picture of Mr. Whiskerson",
+//           phone: "(212) 555-1234",
+//           email: "mr.whiskaz@catnap.meow"
+//         }}
+//       /> OBJECT PROPS */}
       
-      {contactComponents}
-    </div>
-  );
-};
+//       {contactComponents}
+//     </div>
+//   );
+// };
 
 export default App;
