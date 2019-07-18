120a3116d00b3a39f6430a6a658770ebbc9c6acb
Robert Kramer <robert.n.kramer@gmail.com>
2019-07-18 15:59:19 -0700

    current

--- src/App.js
+++ src/App.js
@@ -24,41 +24,4 @@ class App extends Component {
   }
 }
 
-// const App = () => {
-//   const contactComponents = contactData.map(contact => {
-//     return (
-//       <ContactCard 
-//       key={contact.id} 
-//       name={contact.name} 
-//       imgUrl={contact.imgUrl} 
-//       imgAlt={contact.imgAlt}
-//       phone={contact.phone}
-//       email={contact.email} />
-//     )
-//   });
-
-//   return (
-//     <div className="contacts">
-//       {/* <ContactCard
-//         name="Mr. Whiskerson"
-//         imgUrl="http://placekitten.com/300/200"
-//         imgAlt="Picture of Mr. Whiskerson"
-//         phone="(212) 555-1234"
-//         email="mr.whiskaz@catnap.meow"
-//       /> INDIVIDUAL PROPS */}
-//       {/* <ContactCard
-//         contact={{
-//           name: "Mr. Whiskerson",
-//           imgUrl: "http://placekitten.com/300/200",
-//           imgAlt: "Picture of Mr. Whiskerson",
-//           phone: "(212) 555-1234",
-//           email: "mr.whiskaz@catnap.meow"
-//         }}
-//       /> OBJECT PROPS */}
-      
-//       {contactComponents}
-//     </div>
-//   );
-// };
-
 export default App;
--- src/components/ContactCards.js
+++ src/components/ContactCards.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 const ContactCard = (props) => {
     return (
-        // <div className="contact-card">
-        //     <img src={props.contact.imgUrl} alt={props.contact.imgAlt}/>
-        //     <h3>{props.contact.name}</h3>
-        //     <p>Phone: {props.contact.phone}</p>
-        //     <p>Email: {props.contact.email}</p>
-        // </div>
         <div className="contact-card">
             <img src={props.imgUrl} alt={props.imgAlt}/>
             <h3>{props.name}</h3>
