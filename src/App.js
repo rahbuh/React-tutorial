d01a34030a9114bcd407af09fd84c306901827c2
Robert Kramer <robert.n.kramer@gmail.com>
2019-07-19 19:44:10 -0700

    core from memory

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React, { Component } from "react";
+import ContactCards from "./components/ContactCards";
+import contactData from "./contactData";
 
+class App extends Component {
+  state = {
+    contacts: []
+  };
+
+  componentDidMount() {
+    this.setState({ contacts: contactData });
+  }
+
+  render() {
+    const contacts = this.state.contacts;
+    const contactInfo = contacts.map(contact => (
+      <ContactCards key={contact.id} contact={contact} />
+    ));
+
+    return <div className={"contacts"}>{contactInfo}</div>;
+  }
+}
+
+export default App;
--- src/components/ContactCards.js
+++ src/components/ContactCards.js
@@ -1,1 +1,14 @@
+import React from "react";
 
+function ContactCards({contact: {name, imgUrl, imgAlt, phone, email}}) {
+  return (
+    <div className={'contact-card'}>
+      <img src={imgUrl} alt={imgAlt} />
+      <h3>{name}</h3>
+      <p>Phone: {phone}</p>
+      <p>Email: {email}</p>
+    </div>
+  );
+}
+
+export default ContactCards;
